fbc57d647fe200cf176462f8ceb709e3651654c6
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-13 21:41:27 +0200

    Set a splash screen for 2s

--- App.js
+++ App.js
@@ -3,19 +3,38 @@ import { StyleSheet, Text, View } from 'react-native'
 import SplashScreen from './Components/SplashScreen'
 
 export default class App extends Component {
+
   constructor(props) {
     super(props)
     this.state = {
       isLoading: true
     }
   }
+
+  performTimeConsumingTask = async() => {
+    return new Promise((resolve) => 
+      setTimeout(
+        () => { resolve('result') },
+        2000
+      )
+    )
+  }
+
+  async componentDidMount() {
+    const data = await this.performTimeConsumingTask()
+
+    if (data !== null) {
+      this.setState({ isLoading: false })
+    }
+  }
+
   render() {
     if (this.state.isLoading) {
       return <SplashScreen />
     }
     return (
       <View style={styles.main_container}>
-        <Text>
+        <Text style={styles.text_container}>
           Welcome To RunRun : Your favorite running mobile app
         </Text>
       </View>
@@ -25,9 +44,11 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   main_container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  text_container: {
+    fontSize: 20,
+    color: 'orange'
   }
 })
