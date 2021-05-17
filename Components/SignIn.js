adf35c664c6e584444528a9208e33e5a55c966f5
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-05-17 19:31:18 +0200

    Add social icons for Signing in

--- Components/SignIn.js
+++ Components/SignIn.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import { SocialIcon } from 'react-native-elements'
 
 export default function SignIn({ navigation }) {
     return ( 
         <View style={styles.main_container}>
-          <Text>Sign In Screen</Text>
+          <SocialIcon
+            title='Continue with Google'
+            button 
+            type='google'
+            style={{ width: '80%' }}
+          />
+          <SocialIcon
+            title='Continue with Facebook'
+            button 
+            type='facebook'
+            style={{ width: '80%' }}
+          />
+          <SocialIcon
+            title='Continue with Twitter'
+            button 
+            type='twitter'
+            style={{ width: '80%' }}
+          />
         </View>
     )
 }
