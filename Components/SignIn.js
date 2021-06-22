3c8c2de90e1c9b0976a98a561639b24001282bc9
Amine Aouragh <71039249+AmineAouragh@users.noreply.github.com>
2021-06-22 22:05:03 +0100

    Add files via upload

--- Components/SignIn.js
+++ Components/SignIn.js
@@ -10,6 +10,28 @@ export default function SignIn({ navigation }) {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [isValid, setIsValid] = useState(false)
+    const [loggedIn, setloggedIn] = useState(false)
+
+    _signIn = async () => {
+      try {
+        await GoogleSignin.hasPlayServices();
+        const {accessToken, idToken} = await GoogleSignin.signIn();
+        setloggedIn(true);
+      } catch (error) {
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+          // user cancelled the login flow
+          alert('Cancel');
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+          alert('Signin in progress');
+          // operation (f.e. sign in) is in progress already
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+          alert('PLAY_SERVICES_NOT_AVAILABLE');
+          // play services not available or outdated
+        } else {
+          // some other error happened
+        }
+      }
+    };
 
     return ( 
         <View style={styles.main_container}>
